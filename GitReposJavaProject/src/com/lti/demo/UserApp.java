09741c1292b7d353c373dc8b41e0a4c2f5945238
AnishTiwari <anishtiwari@git.om>
2021-09-15 15:27:13 +0530

    New Java Product Message

--- GitReposJavaProject/src/com/lti/demo/UserApp.java
+++ GitReposJavaProject/src/com/lti/demo/UserApp.java
@@ -6,6 +6,8 @@ public class UserApp {
 	public static void main(String[] args) {
 		Product p=new Product(101,"pen",100);
 		System.out.println(p);
+		System.out.println(p.getCost());
+		System.out.println(p.getCost());
 	}
 
 }
